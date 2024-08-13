f3f6aa28d42c548726ab34df100fb6f43457c662
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-08-13 22:07:44 +0530

    models-intregated

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Welcome')
+    res.send("hey");
 })
 
-app.listen(3000, () => {
-    console.log('server started!')
-})
+app.listen(3000);
